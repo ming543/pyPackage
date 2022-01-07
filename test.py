538f3ad1bb1e04b602184099d3ed70948945f901
SamLee <sam.lee@efcotec.com>
2022-01-07 12:03:32 +0800

    pexpect_update

--- test.py
+++ test.py
@@ -1,25 +1,22 @@
 #!/bin/python3
 #from subprocess import Popen, PIPE
 import sys
+import pexpect
 #sys.path.append("..")
 from pyFunc import moduleSys
 
-print('filename: ' + __file__)
+#print('filename: ' + __file__)
+
+process = pexpect.spawn('df -h')
+#process.logfile = sys.stdout
+
+#print(process.read('sda1'))
+print(process.read())
+print(process.expect(pexpect.EOF))
+
+
+
 
 #moduleSys.getCpuMips()
 #print(n)
-#!/usr/bin/python
-import libtmux
-server = libtmux.Server()
-session = server.find_where({"session_name": "session_test"})
-
-session = server.new_session(session_name="session_test", kill_session=True, attach=False)
-window = session.new_window(attach=True, window_name="session_test")
-pane1 = window.attached_panepane2 = window.split_window(vertical=False)
-pane2 = window.split_window(vertical=False)
-window.select_layout('even-horizontal')
-pane1.send_keys('ls -al')
-pane2.send_keys('ls -al')
-
-server.attach_session(target_session="session_test")
 
