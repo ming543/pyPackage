57d98fc04fdca43cc9c9123be206cf5e36543348
SamLee <sam.lee@efcotec.com>
2022-02-08 11:49:49 +0800

    os_clone

--- master.py
+++ master.py
@@ -33,9 +33,10 @@ else:
 
 def mMenu():
     m0 = '製作測試碟64G Build test disk'
+    m1 = '複製OS印象檔至測試碟 Sync OS image to test disk'
     m7 = '更新本機測試程式 Update Test Script'
     ml = '系統關機 Power off system'
-    options = [m0, m7, ml]
+    options = [m0, m1, m7, ml]
 
     os.system('clear')
     print(Fore.YELLOW + "%s 主選單 MAIN-MENU" % booted + Fore.RESET, end='')
@@ -45,6 +46,8 @@ def mMenu():
 
     if choice == m0:  # Build test disk
         testerBuild()
+    if choice == m1:  # Build test disk
+        osSync()
     elif choice == m7:  # Update Linux script
         gitPull()
     # Last of list
@@ -54,8 +57,7 @@ def mMenu():
         os.system('systemctl poweroff')
 
 
-#build test disk
-def testerBuild():
+def diskChoose():
     os.system('clear')
     output = subprocess.check_output(
             'lsblk -o type,name,model,size', shell=True)
@@ -67,37 +69,77 @@ def testerBuild():
     print("選取回寫入儲存裝置(克隆目標) ")
     diskShow = enquiries.choose(' Choose clone disk options: ', options)
     diskGet = diskShow.split(' ')[1]
+    return diskGet
 
-    osGet = ""
+def osChoose():
+    os.system('clear')
+    index = []
+    osFolder = "/home/partimag/OS_TESTER"
+    os.system('clear')
+    for filename in os.listdir(osFolder):
+        index += [filename]
+    try:
+        print("選取回寫作業系統 ")
+        osGet = enquiries.choose(' Choose clone OS options: ', index)
+    except ValueError:
+        print(Fore.YELLOW + "未發現作業系統 No OS find " + Fore.RESET, end='')
+        print("請與工程確認...")
+        print(" ")
+        osGet = "NO_OS"
+        input("按任意鍵繼續 Press any key continue...")
+        mMenu()
+
+    return osGet
+#    with shelve.open('/home/stux/pyPackage/dataBase') as db:
+#        db['osSave'] = osGet
+
+
+def passGreen():
+    print(Fore.GREEN + "PPPPP_______A______SSSSSS___SSSSSS" + Fore.RESET)
+    print(Fore.GREEN + "PP__PP____AA_AA____SS_______SS____" + Fore.RESET)
+    print(Fore.GREEN + "PP___PP__AA___AA___SS_______SS____" + Fore.RESET)
+    print(Fore.GREEN + "PP__PP___AA___AA___SSSSSS___SSSSSS" + Fore.RESET)
+    print(Fore.GREEN + "PPPPP____AA___AA_______SS_______SS" + Fore.RESET)
+    print(Fore.GREEN + "PP_______AA_A_AA_______SS_______SS" + Fore.RESET)
+    print(Fore.GREEN + "PP_______AA___AA___SSSSSS___SSSSSS" + Fore.RESET)
+    print(" ")
+    print("按任意鍵關機,n鍵返回主選單  ", end='')
+    check = input("Press any key power off").lower()
+    if check == ("n"):
+        mMenu()
+    os.system('systemctl poweroff')
+
+def failRed():
+    print(Fore.RED + "FFFFFF______A______IIIIII____LL____" + Fore.RESET)
+    print(Fore.RED + "FF_______AA___AA_____II______LL____" + Fore.RESET)
+    print(Fore.RED + "FFFF_____AA___AA_____II______LL____" + Fore.RESET)
+    print(Fore.RED + "FF_______AA___AA_____II______LL____" + Fore.RESET)
+    print(Fore.RED + "FF_______AA_A_AA_____II______LL____" + Fore.RESET)
+    print(Fore.RED + "FF_______AA___AA___IIIIII____LLLLLL" + Fore.RESET)
+    print(" ")
+    print("按任意鍵關機,n鍵返回主選單  ", end='')
+    check = input("Press any key power off").lower()
+    if check == ("n"):
+        mMenu()
+    os.system('systemctl poweroff')
+
+
+#build test disk
+def testerBuild():
+    diskChoose()
+    osChoose()
+#    osGet = osGet()
     osClone = subprocess.call(
             "sudo /usr/sbin/ocs-sr -g auto -e1 auto -e2 -r -j2 -k1 -scr -icds -p command restoredisk OS_TESTER/%s %s" %(osGet, diskGet), shell=True)
     if osClone != 0:
-        print(Fore.RED + "FFFFFF______A______IIIIII____LL____" + Fore.RESET)
-        print(Fore.RED + "FF_______AA___AA_____II______LL____" + Fore.RESET)
-        print(Fore.RED + "FFFF_____AA___AA_____II______LL____" + Fore.RESET)
-        print(Fore.RED + "FF_______AA___AA_____II______LL____" + Fore.RESET)
-        print(Fore.RED + "FF_______AA_A_AA_____II______LL____" + Fore.RESET)
-        print(Fore.RED + "FF_______AA___AA___IIIIII____LLLLLL" + Fore.RESET)
-        print(" ")
-        print("按任意鍵關機,n鍵返回主選單  ", end='')
-        check = input("Press any key power off").lower()
-        if check == ("n"):
-            mMenu()
-        os.system('systemctl poweroff')
+        failRed()
     else:
-        print(Fore.GREEN + "PPPPP_______A______SSSSSS___SSSSSS" + Fore.RESET)
-        print(Fore.GREEN + "PP__PP____AA_AA____SS_______SS____" + Fore.RESET)
-        print(Fore.GREEN + "PP___PP__AA___AA___SS_______SS____" + Fore.RESET)
-        print(Fore.GREEN + "PP__PP___AA___AA___SSSSSS___SSSSSS" + Fore.RESET)
-        print(Fore.GREEN + "PPPPP____AA___AA_______SS_______SS" + Fore.RESET)
-        print(Fore.GREEN + "PP_______AA_A_AA_______SS_______SS" + Fore.RESET)
-        print(Fore.GREEN + "PP_______AA___AA___SSSSSS___SSSSSS" + Fore.RESET)
-        print(" ")
-        print("按任意鍵關機,n鍵返回主選單  ", end='')
-        check = input("Press any key power off").lower()
-        if check == ("n"):
-            mMenu()
-        os.system('systemctl poweroff')
+        passGreen()
+
+def osSync():
+    subprocess.check_call("sudo mount /dev/%s2 /mnt -o umask=000" % diskGet, shell=True, stdin=sys.stdin)
+    subprocess.check_call("sudo rsync -avh /home/partimag/OS_IMAGE /mnt/OS_IMAGE", shell=True, stdin=sys.stdin)
+    subprocess.check_call("sudo umount /mnt", shell=True, stdin=sys.stdin)
 
 #linuxUpdate
 def gitPull():
@@ -113,6 +155,9 @@ def gitPull():
             g.clean('-f', '-d')
             g.pull()
             print("gitPullDone")
+            with open(startTest, "w") as f:
+                f.write("cd /home/stux/pyPackage && python3 master.py")
+            #subprocess.call("sh %s" % startTest, shell=True)
             time.sleep(3)
             break
         else:
