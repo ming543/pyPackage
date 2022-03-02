a4f77fb1151246fbfd5a4d7ad79371ae64a8395c
ming <ming543@gmail.com>
2022-03-02 15:42:40 +0800

    Update moduleSys.py

--- pyFunc/moduleSys.py
+++ pyFunc/moduleSys.py
@@ -66,23 +66,43 @@ def audioWire():
     CHANNELS = 2
     RATE = 44100
     RECORD_SECONDS = 5
-
     p = pyaudio.PyAudio()
-
     stream = p.open(format=p.get_format_from_width(WIDTH),
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 output=True,
                 frames_per_buffer=CHUNK)
-
     print("* recording")
-
     for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
         data = stream.read(CHUNK)
         stream.write(data, CHUNK)
-
     print("* done")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+def audioCallback():
+    WIDTH = 2
+    CHANNELS = 2
+    RATE = 44100
+
+    p = pyaudio.PyAudio()
+
+    def callback(in_data, frame_count, time_info, status):
+        return (in_data, pyaudio.paContinue)
+
+    stream = p.open(format=p.get_format_from_width(WIDTH),
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                output=True,
+                stream_callback=callback)
+
+    stream.start_stream()
+
+    while stream.is_active():
+    time.sleep(0.1)
 
     stream.stop_stream()
     stream.close()
