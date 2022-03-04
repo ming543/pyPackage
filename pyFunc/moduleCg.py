5c2a21996b6ce1a8d2b583026e993dd3f9321f0a
ming <ming543@gmail.com>
2022-03-04 09:43:21 +0800

    Update moduleCg.py

--- pyFunc/moduleCg.py
+++ pyFunc/moduleCg.py
@@ -12,24 +12,22 @@ def cgTest():
     result = cg.boardGetInfo()
     print (result)
     print (result['szSerialNumber'])
-
     pyFolder = "/home/stux/pyPackage/"
     cgFolder = pyFolder + "tools/CgosPy_1_2"
 
 def i2cGpio():
+    print('GPIO testing ......')
     gpio = "/home/stux/pyPackage/tools/cgoslx-gpio/CgosI2cGpio/Lx/cgosi2c_gpio"
     response = subprocess.check_output(gpio, shell=True)
-    response = response.splitlines()
-    print(response)
-    response = str(response).lstrip('b\'').split('\\n')[0]
-    print(response)
-    time.sleep(3)
+    response = response.splitlines()[-1]
+    response = str(response).lstrip('b\'').rstrip('\'')
+    print('GPIO test done ' + response)
     if re.search('input:80', response):
         logging.info('GPIO Loopback Test : PASS')
         return True
     else:
-        logging.error('GPIO Loopback Test : Fail ' + response[0] ' SPEC: 80')
-        failRed("GPIO Loopback test Fail " + response[0])
+        logging.error('GPIO Loopback Test : Fail, Input SPEC: 80 ' + response)
+        failRed("GPIO Loopback test Fail " + response)
     
         
     
