d0ecc5ec826dd58273373937ff999ac7e2101716
SamLee <sam.lee@efcotec.com>
2022-03-17 13:43:35 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import sys
 import os
 
 REQUIRED = {
-  'GitPython', 'enquiries', 'colorama', 'getch', 'getmac' 
-  'py-cpuinfo', 'netifaces', 'pyusb', 'pyserial'
+  'GitPython', 'enquiries', 'colorama', 'getch', 'getmac', 
+  'py-cpuinfo', 'netifaces', 'pyusb', 'pyserial', 'pynum'
 }
 
 installed = {pkg.key for pkg in pkg_resources.working_set}
