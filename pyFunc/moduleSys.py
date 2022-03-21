db63f07c3885161adee0424d58cea6fc38b7708f
ming <ming543@gmail.com>
2022-03-21 14:38:39 +0800

    Update moduleSys.py

--- pyFunc/moduleSys.py
+++ pyFunc/moduleSys.py
@@ -62,8 +62,8 @@ def dosPull():
             subprocess.call("cd %s && sudo git checkout origin/master -- V23C" % dosFolder, shell=True)
             subprocess.call("cd %s && sudo git checkout origin/master -- AICCFG" % dosFolder, shell=True)
             print("gitDosPullDone")
+            #copy efiScript to DOS folder
             subprocess.call("sudo cp -r %sefiScript %s" % (pyFolder, dosFolder), shell=True)
-
             rC = subprocess.call(
                 "cd %s && sudo find . -type f \( -name '*.BAT' -o -name '*.TXT' \) -exec todos -v '{}' \;" % dosFolder, shell=True)
             if rC == 0:  # check rclone pass or fail
