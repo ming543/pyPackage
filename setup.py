9eaea41892eddf913d02119f018de638be3a7672
SamLee <sam.lee@efcotec.com>
2022-07-19 11:25:46 +0800

    pythonSetup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 REQUIRED = {
   'GitPython', 'enquiries', 'colorama', 'getch', 'getmac', 
   'py-cpuinfo', 'netifaces', 'pyusb', 'pyserial', 'numpy',
-  'sh', 'scp', 'paramiko', 'ntplib'
+  'sh', 'scp', 'paramiko', 'ntplib', 'jedi'
 }
 
 installed = {pkg.key for pkg in pkg_resources.working_set}
